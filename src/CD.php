6462d00d82da29c6c56bb494e6f5ad5e50c71122
William Swanson & Christopher Calascione <student@epicodus.com>
2015-08-06 15:06:51 -0700

    initial set up and create cd object

---
+++ src/CD.php
@@ -0,0 +1,37 @@
+<?php
+class CD
+{
+    private $artist;
+
+
+    function __construct($band_name)
+    {
+        $this->artist = $band_name;
+    }
+
+    function setArtist($new_band_name)
+    {
+        $this->artist = $new_band_name;
+    }
+
+    function getArtist()
+    {
+        return $this->artist;
+    }
+
+    function save()
+    {
+        array_push($_SESSION['list_of_artists'], $this);
+    }
+
+    static function getAll()
+    {
+        return $_SESSION['list_of_artists'];
+    }
+
+    static function deleteAll()
+    {
+        $_SESSION['list_of_artists'] = array();
+    }
+}
+ ?>
