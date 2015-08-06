7e87e7e25c23d64b089dd1a682723f8d635790b8
William Swanson & Christopher Calascione <student@epicodus.com>
2015-08-06 16:06:40 -0700

    start on / route

--- app/app.php
+++ app/app.php
@@ -2,10 +2,18 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/CD.php";
 
-    $app = new Silex/Application();
+    session_start();
 
-    $app->get("/", function() {
-        return "";
+    if (empty($_SESSION['list_of_artists'])) {
+        $_SESSION['list_of_artists'] = array();
+    }
+
+    $app = new Silex\Application();
+    $app['debug'] = false;
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__."/../views"));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('home.html.twig', array('artists' => CD::getAll()));
     });
 
     return $app;
